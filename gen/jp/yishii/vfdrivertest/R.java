69b31efd3a4991a4e9eee799c555ed83ec0ea99a
Yasuhiro ISHII <ishii.yasuhiro@gmail.com>
2012-10-31 08:16:33 +0900

    update relates

--- gen/jp/yishii/vfdrivertest/R.java
+++ gen/jp/yishii/vfdrivertest/R.java
@@ -16,6 +16,8 @@ public final class R {
     public static final class id {
         public static final int button1=0x7f060000;
         public static final int button2=0x7f060001;
+        public static final int button3=0x7f060002;
+        public static final int webview1=0x7f060003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
